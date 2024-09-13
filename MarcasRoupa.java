7c13c732345b7c9f6e30655d6cc1b70680fd6ded
gabivdcamargo <159277563+gabivdcamargo@users.noreply.github.com>
2024-09-13 12:04:01 -0300

    Primeiro Commit

---
+++ MarcasRoupa.java
@@ -0,0 +1,15 @@
+package EnumRoupa;
+
+public enum MarcasRoupa {
+
+	COLCCI,
+	GUCCI,
+	NIKE,
+	VANS,
+	CHAMPION,
+	FARM,
+	CALVIN_KLEIN
+
+}
+
+
---
+++ PrincipalRoupa.java
@@ -0,0 +1,26 @@
+package EnumRoupa;
+
+public class PrincipalRoupa {
+
+	public static void main(String[] args) {
+		MarcasRoupa roupa1 = MarcasRoupa.COLCCI;
+		MarcasRoupa roupa2 = MarcasRoupa.GUCCI;
+		MarcasRoupa roupa3 = MarcasRoupa.NIKE;
+		MarcasRoupa roupa4 = MarcasRoupa.VANS;
+		MarcasRoupa roupa5 = MarcasRoupa.CHAMPION;
+		MarcasRoupa roupa6 = MarcasRoupa.FARM;
+		MarcasRoupa roupa7 = MarcasRoupa.CALVIN_KLEIN;
+
+		System.out.println(roupa1);
+		System.out.println(roupa2);
+		System.out.println(roupa3);
+		System.out.println(roupa4);
+		System.out.println(roupa5);
+		System.out.println(roupa6);
+		System.out.println(roupa7);
+	}
+
+
+}
+
+
